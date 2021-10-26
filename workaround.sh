373423eb367ed5a78580cc68b13b62a0c9b02c76
Weston Schmidt <Weston_Schmidt@alumni.purdue.edu>
2021-10-25 22:04:35 -0700

    Graft enough meson build scripts onto the rbus libraries so we can build cpeabs.

--- workaround.sh
+++ workaround.sh
@@ -3,6 +3,14 @@
 mkdir -p subprojects/packagefiles
 
 pushd wrapdb
+
 tar -czf rtMessage.tar.gz rtMessage
-mv rtMessage.tar.gz ../subprojects/packagefiles/.
+mv -f rtMessage.tar.gz ../subprojects/packagefiles/.
+
+tar -czf rbuscore.tar.gz rbuscore
+mv -f rbuscore.tar.gz ../subprojects/packagefiles/.
+
+tar -czf rbus.tar.gz rbus
+mv -f rbus.tar.gz ../subprojects/packagefiles/.
+
 popd
