09918150252f114fd57c5edae383711219fe2e1f
BalaNaveenaNivetha <105800187+BalaNaveenaNivetha@users.noreply.github.com>
2022-06-15 18:03:07 +0530

    test: Modify Webconfig and Supplementary URLs

--- src/rdkv/impl.c
+++ src/rdkv/impl.c
@@ -28,7 +28,7 @@
 #include <rbus-core/rbus_session_mgr.h>
 #include <stdio.h>
 #include <sys/sysinfo.h>
-#include <json/json.h>
+#include "cpeabs_ovsdb_utils.h"
 #include "cpeabs.h"
 /*----------------------------------------------------------------------------*/
 /*                                   Macros                                   */
