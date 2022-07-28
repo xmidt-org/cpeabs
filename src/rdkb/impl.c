fbcd01b5db5383226d4aa8ceac54e644fa5d07a9
vels-02 <37198348+vels-02@users.noreply.github.com>
2022-07-28 16:43:45 +0530

    Update impl.c
    Instead of using the product name (_WNXL11BWL_PRODUCT_REQ_).  , used a feature-based ( RDKB_EXTENDER)  check for DEVICE_MAC retrieval.

--- src/rdkb/impl.c
+++ src/rdkb/impl.c
@@ -53,7 +53,7 @@
 #define DEVICE_MAC                   "Device.DeviceInfo.X_COMCAST-COM_WAN_MAC"
 #elif defined(_HUB4_PRODUCT_REQ_)
 #define DEVICE_MAC                   "Device.DeviceInfo.X_COMCAST-COM_WAN_MAC"
-#elif defined(_WNXL11BWL_PRODUCT_REQ_)
+#elif defined(RDKB_EXTENDER)
 #define DEVICE_MAC                   "Device.DeviceInfo.X_COMCAST-COM_WAN_MAC"
 #else
 #define DEVICE_MAC                   "Device.X_CISCO_COM_CableModem.MACAddress"
