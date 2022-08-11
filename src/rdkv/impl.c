641b7e498d915bc125e62961c47baead0c0dbdb2
Bala Naveena M <balanaveena_murugesan@comcast.com>
2022-08-11 20:46:42 +0530

    RDK-36782: Cpeabs changes for RDK-V devices - webconfig json file
    Reason for change: Build Webconfig as bin file,
    modify cpeabs to fetch rdk-v code, json file.
    Test Procedure: Build and test Webconfig
    Risks: Low
    
    Signed-off-by: Bala Naveena M <balanaveena_murugesan@comcast.com>

--- src/rdkv/impl.c
+++ src/rdkv/impl.c
@@ -51,7 +51,7 @@
 #define PARAM_RFC_ENABLE "eRT.com.cisco.spvtg.ccsp.webpa.WebConfigRfcEnable"
 
 #define WEBCFG_PARTNER_JSON_FILE "/etc/partners_defaults_webcfg_video.json"
-#define WEBCFG_DB_STORE          "/opt/.webcfg.json"
+#define WEBCFG_DB_STORE          "/opt/.webconfig.json"
 
 #define RETURN_OK 0
 #define RETURN_ERR -1
