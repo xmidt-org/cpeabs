0a96533bccc53ca7371318451aa61f8adf49aaa8
mmikhi643 <Mikhil_Mikhil@comcast.com>
2023-02-15 16:00:48 +0000

    RDKC-13035 : Remove camera specific patch from cpeabs
    Reason: removing unecessary checks for DEVICE_CAMERA

--- src/rdkc/impl.c
+++ src/rdkc/impl.c
@@ -202,13 +202,8 @@ char * getDeviceBootTime()
 
 char * getProductClass()
 {
-#ifndef DEVICE_CAMERA
-    char *productClass = NULL;
-    productClass = getParamValue(PRODUCT_CLASS);
-#else
     char *productClass = (char *) malloc(sizeof(char) * strlen("CAMERA") + 1);
     strcpy(productClass, "CAMERA");
-#endif
 
     if(productClass)
       WebcfgDebug("productClass returned from lib is %s\n", "CAMERA");
